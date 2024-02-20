66436bfe1f23b297de78dc3361b8e8bf292814af
Yehonatan Cohen Scaly <scalys7@gmail.com>
2024-02-20 12:37:57 +0200

    Expose exp_iter

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,7 @@ pub use crate::errors::ProofError;
 pub use crate::generators::{BulletproofGens, BulletproofGensShare, PedersenGens};
 pub use crate::linear_proof::LinearProof;
 pub use crate::range_proof::RangeProof;
+pub use util::exp_iter;
 
 #[cfg_attr(feature = "docs", doc(include = "../docs/aggregation-api.md"))]
 pub mod range_proof_mpc {
